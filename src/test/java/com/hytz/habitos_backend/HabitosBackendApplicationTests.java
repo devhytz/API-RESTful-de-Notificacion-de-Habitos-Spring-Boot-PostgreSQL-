0cb3d75c7470ff8e0202bec950c19394255d6983
devhytz <devhytz@gmail.com>
2026-08-13 16:39:03 -0500

    First commit

---
+++ src/test/java/com/hytz/habitos_backend/HabitosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hytz.habitos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
