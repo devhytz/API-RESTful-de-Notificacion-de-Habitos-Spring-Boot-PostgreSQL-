c1be995bd011e92c423fa68b96584515160dcbd0
devhytz <devhytz@gmail.com>
2026-08-21 14:26:30 -0500

    Refact: Manejo global de excepciones

--- src/main/java/com/hytz/habitos_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hytz/habitos_backend/exception/GlobalExceptionHandler.java
@@ -14,31 +14,9 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> gestionarErroresDeValidacion(MethodArgumentNotValidException ex) {
-
-        // 1. Crear un Map vacío donde guardaremos campo -> mensaje
-        Map<String, String> errores = new HashMap<>();
-
-        // 2. Extraer los errores de la excepción 'ex'
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String campo = ((FieldError) error).getField(); // Nombre del atributo (ej: "email")
-            String mensaje = error.getDefaultMessage();     // Mensaje definido en @Email o @NotBlank
-
-            errores.put(campo, mensaje);
-        });
-
-        // 3. Retornar el mapa con el status HTTP 400
-        return ResponseEntity.badRequest().body(errores);
-    }
-
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, String>> gestionarErroresDeNoEncontrado(ResourceNotFoundException ex) {
-
-        Map<String, String> errores = new HashMap<>();
-
-        errores.put("Mensaje", ex.getMessage());
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errores);
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
+
 }
