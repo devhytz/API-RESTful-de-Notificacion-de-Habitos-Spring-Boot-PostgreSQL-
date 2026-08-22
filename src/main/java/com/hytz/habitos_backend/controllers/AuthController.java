bdddd2909001dba88ef1fa2b7c442e1e6d42f411
devhytz <devhytz@gmail.com>
2026-08-22 13:40:59 -0500

    Feat: Sistema de seguridad completo funcional

--- src/main/java/com/hytz/habitos_backend/controllers/AuthController.java
+++ src/main/java/com/hytz/habitos_backend/controllers/AuthController.java
@@ -1,4 +1,30 @@
 package com.hytz.habitos_backend.controllers;
 
+import com.hytz.habitos_backend.dtos.AuthResponse;
+import com.hytz.habitos_backend.dtos.LoginRequest;
+import com.hytz.habitos_backend.dtos.RegistroRequest;
+import com.hytz.habitos_backend.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<AuthResponse> registrar(@RequestBody RegistroRequest request) {
+        return ResponseEntity.ok(authService.registrar(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
